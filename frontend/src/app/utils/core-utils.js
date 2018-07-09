3ca3b33c25484bc19fc98123188262c17ad82612
nb-ohad <ohad.mitrani@noobaa.com>
2018-07-09 11:03:41 +0300

    Add state$/action$ injection + Connectable base view model

--- frontend/src/app/utils/core-utils.js
+++ frontend/src/app/utils/core-utils.js
@@ -298,8 +298,17 @@ export function get(val, path, defaultValue) {
 }
 
 export function equalItems(arr1, arr2) {
-    return arr1.length === arr2.length &&
-        arr1.every((item, i) => Object.is(item, arr2[i]));
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+
+    for (let i = 0; i < arr1.length; ++i) {
+        if (!Object.is(arr1[i], arr2[i])) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 export function ensureArray(val) {
