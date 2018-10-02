e7edbe3612be17363422653aa1136f4b1cb82768
liranmauda <liran.mauda@noobaa.com>
2018-10-02 10:37:42 +0300

    Add tier_functions.js
    Add tier_functions.js

---
+++ src/test/utils/tier_functions.js
@@ -0,0 +1,31 @@
+/* Copyright (C) 2016 NooBaa */
+'use strict';
+
+class TierFunction {
+
+    constructor(client, report) {
+        this._client = client;
+        this._report = report;
+    }
+    async createTier() {
+
+    }
+
+    async deleteTier() {
+
+    }
+
+    async createTierPolicy() {
+
+    }
+
+    async editTierPolicy() {
+
+    }
+
+    async deleteTierPolicy() {
+
+    }
+}
+
+exports.TierFunction = TierFunction;
